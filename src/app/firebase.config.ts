0598a83b9532d03f545bc3b9b6523f000e5add84
FranciscoReyes19 <francisco.reyesfrt@outlook.com>
2020-03-14 12:55:52 -0600

    kickoff-ionic-restaurant

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,21 @@
+//config default
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdCxdnHd6YgIp6aygCBK5TmZSQ2toKNT4",
+  authDomain: "restaurant-firebase-306c8.firebaseapp.com",
+  databaseURL: "https://restaurant-firebase-306c8.firebaseio.com",
+  projectId: "restaurant-firebase-306c8",
+  storageBucket: "restaurant-firebase-306c8.appspot.com",
+  messagingSenderId: "141252318027",
+  appId: "1:141252318027:web:8f3f1ed113dcff83df0ece",
+  measurementId: "G-BE5H22EEKJ"
+};
+
+// Sandip's firebase configuration
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyAzusy10a0pSk-huxJO-DUtK0RoGgqJcpM",
+//     authDomain: "restaurantweb-ionic-app.firebaseapp.com",
+//     databaseURL: "https://restaurantweb-ionic-app.firebaseio.com",
+//     projectId: "restaurantweb-ionic-app",
+//     storageBucket: "restaurantweb-ionic-app.appspot.com",
+//     messagingSenderId: "120142074708"
+//   };
